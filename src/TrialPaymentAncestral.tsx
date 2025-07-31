58d64d64d76320e0466724caef03ff994911cc29
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-07-31 08:07:35 -0300

    Update TrialPaymentAncestral.tsx

--- src/TrialPaymentAncestral.tsx
+++ src/TrialPaymentAncestral.tsx
@@ -797,6 +797,11 @@ const TrialPaymentAncestral: React.FC<TrialPaymentAncestralProps> = ({
             ← Voltar para seleção de preços
           </button>
         )}
+        
+      <p className="text-gray-700 text-center leading-relaxed">
+                "Desvende o que está escrito e molde o que virá. O verdadeiro poder reside em conhecer seu destino para então dominá-lo." ~São Cipriano c🔱
+              </p>
+        
       </div>
     </div>
   );
